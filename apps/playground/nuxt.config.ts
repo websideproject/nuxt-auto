cfca57118339510fe570801a6621dd589e5ac8ce
Benjamin Gervan <gben9302@gmail.com>
2026-02-09 20:39:08 +0100

    Fixes in the release process

--- apps/playground/nuxt.config.ts
+++ apps/playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   modules: ['@websideproject/nuxt-auto-api', '@websideproject/nuxt-auto-admin'],
   devtools: { enabled: true },
-  autoApi: {},
-  autoAdmin: {},
   compatibilityDate: '2026-02-09',
+  autoAdmin: {},
+  autoApi: {},
 })
